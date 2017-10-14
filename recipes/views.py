564aab613835067922b3bdab2353ac6add0d9313
mostlybadfly <reachdanielnunez@gmail.com>
2017-10-14 07:57:10 -0500

    Added URLs, views and detail template.

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Recipe
 
 def index(request):
-    return HttpResponse("This is the recipe index.")
+    recipe_list = Recipe.objects.all()
+    context = {'recipe_list': recipe_list}
+    return render(request, 'recipes/index.html', context)
+
+def add(request):
+    return HttpResponse("A new recipe is added here.")
+
+def edit(request, recipe_id):
+    return HttpResponse("editing recipe % s." % recipe_id)
+
+def detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'recipes/detail.html', {'recipe': recipe})
