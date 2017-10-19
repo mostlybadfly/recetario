0ade41ac56184e7b3144e48f6f7b8360c83d7b64
mostlybadfly <reachdanielnunez@gmail.com>
2017-10-18 19:29:23 -0500

    Add User Signup/Login/Logout.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
-from django.template import loader
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Recipe
 
@@ -18,3 +19,17 @@ def edit(request, recipe_id):
 def detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     return render(request, 'recipes/detail.html', {'recipe': recipe})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'recipes/signup.html', {'form': form})
