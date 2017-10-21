1ad7b7022675111905abbf7cbf618a38382436ec
mostlybadfly <reachdanielnunez@gmail.com>
2017-10-21 06:56:51 -0500

    Removed Profile from model. Moved signup to registration templates.

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Recipe(models.Model):
@@ -20,12 +19,3 @@ class Instruction(models.Model):
     ordinal = models.IntegerField(blank=True, null=True)
     instruction_text = models.TextField(null=True)
     recipe = models.ForeignKey(Recipe, related_name='instructions')
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-@receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
--- recipes/views.py
+++ recipes/views.py
@@ -32,4 +32,4 @@ def signup(request):
             return redirect('/')
     else:
         form = UserCreationForm()
-    return render(request, 'recipes/signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {'form': form})
