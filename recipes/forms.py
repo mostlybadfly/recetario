576a4adf30b945100b6486abeb15ad2fd4bf03e0
mostlybadfly <reachdanielnunez@gmail.com>
2017-11-21 22:31:28 -0600

    Installed crispy forms, Updated recipe form

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,20 +1,51 @@
 from django.forms import ModelForm, inlineformset_factory
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
 
 from .models import Recipe, Ingredient, Instruction
 
 class RecipeForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-inline'
+        self.helper.form_method = 'post'
 
     class Meta:
         model = Recipe
         fields = ('title', 'cuisine', 'cooking_time', 'servings')
 
 class IngredientForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(IngredientForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-inline'
+        self.helper.form_tag = False
+        self.helper.render_hidden_fields = True
+        self.helper.disable_csrf = True
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout (
+            Field('quantity', placeholder="Quantity"),
+            Field('measurement', placeholder="Measurement"),
+            Field('name', placeholder="Name")
+        )
 
     class Meta:
         model = Ingredient
         fields = ('quantity', 'measurement', 'name')
 
 class InstructionForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(InstructionForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.render_hidden_fields = True
+        self.helper.disable_csrf = True
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout (
+            Field('ordinal', placeholder="Position"),
+            Field('instruction_text', placeholder="Add a Step"),
+        )
 
     class Meta:
         model = Instruction
